0deba779f9e40ef16a0be643719ed3d824162a24
Vladislav_Global <vladalexkirbut2003@gmail.com>
2023-02-23 18:28:19 +0300

    Added enum Currency

---
+++ src/teachmeskills/vlad/hw14/currencytype/Currency.java
@@ -0,0 +1,19 @@
+package teachmeskills.vlad.hw14.currencytype;
+
+public enum Currency {
+    BYN ("Белорусский рубль"),
+    USD ("Доллар США"),
+    EUR ("Евро"),
+    GBP ("Фунт стерлингов"),
+    RUB ("Российский рубль"),
+    CNY ("Китайский юань");
+
+    private final String russianNameOfCurrency;
+    Currency (String russianNameOfCurrency) {
+        this.russianNameOfCurrency = russianNameOfCurrency;
+    }
+
+    public String getNumericValue() {
+        return russianNameOfCurrency;
+    }
+}
